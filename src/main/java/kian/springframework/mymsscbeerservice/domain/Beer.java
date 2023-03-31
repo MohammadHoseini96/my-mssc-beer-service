b844fb83a0bbd251385dcb047fa8a5f5bebe4dc6
Kian <MohammadHoseini96>
2023-03-31 17:48:55 +0330

    adding Beer entity. closes #5

---
+++ src/main/java/kian/springframework/mymsscbeerservice/domain/Beer.java
@@ -0,0 +1,53 @@
+package kian.springframework.mymsscbeerservice.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/**
+ * @author Kian
+ * @created 3/31/2023 - 5:15 PM
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+
+    private String beerName;
+
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer minOnHand;
+
+    private Integer quantityToBrew;
+
+}
