087e5b790d4f63bc4f5e8f16de8dca0f9e6b589b
Kian <MohammadHoseini96>
2023-04-01 16:30:09 +0330

    adding bean validation for BeerDto . closes #8

--- src/main/java/kian/springframework/mymsscbeerservice/web/model/BeerDto.java
+++ src/main/java/kian/springframework/mymsscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -15,18 +19,30 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class BeerDto {
+
+    // We don't want the client to change these four properties so we use @null
+    @Null
     private UUID id;
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyleEnum;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @Positive
+    @NotNull
     private BigDecimal price;
 
     private Integer quantityOnHand;
